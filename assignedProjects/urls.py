45751d4ebad3abb5260157d67bf02f61d3cfd7fc
pramod.kumar-kiwi <pramod.kumar@kiwitech.com>
2020-06-22 19:04:00 +0530

    dashboard done

--- assignedProjects/urls.py
+++ assignedProjects/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 from kiwiProject import settings
 from . import views
+from assignedProjects.views import (LoginJira)
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', LoginJira.as_view(), name="jira_login"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- assignedProjects/views.py
+++ assignedProjects/views.py
@@ -1,11 +1,49 @@
-from django.shortcuts import render
+"""
+ Views for authentication app.
+"""
 
-# Create your views here.
+# python import
 
-from django.http import HttpResponse
+# django imports
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.views.generic import TemplateView
+from jira import JIRA
 
 
-def login(request):
+class LoginJira(TemplateView):
+    """
+    Views used  for User Registration Success page
+    """
+    template_name = 'index.html'
 
-    return render(request, 'index.html')
-    #return HttpResponse("Hello, world. You're at the polls index.")
+    def get(self, request, **kwargs):
+        """
+        Method to load login page
+        :param kwargs: keyword argument
+        :param request: request argument dict type
+        :return:Sign up success page
+        """
+        return render(request, self.template_name, {
+        })
+
+    def post(self, request, **kwargs):
+        """
+         Method used for resend email for verification link
+        :param request: argument dict type
+        :param kwargs: argument dict type
+        :return: redirect to self page
+        """
+        self.template_name = 'jira_project.html'
+        data = request.POST
+
+        jira_options = {'server': 'https://jira.kiwitech.com/'}
+        jira = JIRA(options=jira_options, basic_auth=(data['username'], data['password']))
+        projects = jira.projects()
+
+        return render(request, self.template_name, {
+            'user': request.user,
+            'projects': projects
+        })
+
+
