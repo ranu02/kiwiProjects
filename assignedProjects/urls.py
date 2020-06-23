9e625284490bbc1df0d567cc5e8aefa695fbca00
pramod.kumar-kiwi <pramod.kumar@kiwitech.com>
2020-06-23 18:34:31 +0530

    session data is store

--- assignedProjects/urls.py
+++ assignedProjects/urls.py
@@ -6,8 +6,10 @@ from django.conf.urls.static import static
 
 from kiwiProject import settings
 from . import views
-from assignedProjects.views import (LoginJira)
+from assignedProjects.views import (LoginJira, JiraDashboard, LogoutView)
 
 urlpatterns = [
     path('', LoginJira.as_view(), name="jira_login"),
+    path('dashboard', JiraDashboard.as_view(), name="dashboard"),
+    path('logout', LogoutView.as_view(), name="logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- assignedProjects/views.py
+++ assignedProjects/views.py
@@ -7,8 +7,9 @@
 # django imports
 from django.contrib import messages
 from django.shortcuts import redirect, render
-from django.views.generic import TemplateView
-from jira import JIRA
+from django.views.generic import TemplateView, View
+from jira import JIRA, JIRAError
+import logging
 
 
 class LoginJira(TemplateView):
@@ -24,6 +25,8 @@ class LoginJira(TemplateView):
         :param request: request argument dict type
         :return:Sign up success page
         """
+        if request.session.has_key('username') and request.session.has_key('password'):
+            return redirect('dashboard')
         return render(request, self.template_name, {
         })
 
@@ -34,16 +37,66 @@ class LoginJira(TemplateView):
         :param kwargs: argument dict type
         :return: redirect to self page
         """
-        self.template_name = 'jira_project.html'
         data = request.POST
+        jira_options = {'server': 'https://jira.kiwitech.com/', 'verify': True}
+        try:
+            JIRA(options=jira_options, basic_auth=(data['username'], data['password']), max_retries=0)
+            # save session with state full
+            request.session['username'] = data['username']
+            request.session['password'] = data['password']
+        except JIRAError as e:
+            logging.error("Failed to connect to JIRA: %s" % e)
+            return render(request, self.template_name, {
+                'wrong_msg': True
+            })
+        return redirect('dashboard')
 
-        jira_options = {'server': 'https://jira.kiwitech.com/'}
-        jira = JIRA(options=jira_options, basic_auth=(data['username'], data['password']))
-        projects = jira.projects()
 
-        return render(request, self.template_name, {
-            'user': request.user,
-            'projects': projects
-        })
+class JiraDashboard(TemplateView):
+    """
+    Views used  for User Registration Success page
+    """
+    template_name = 'jira_project.html'
+
+    def get(self, request, **kwargs):
+        """
+        Method to load login page
+        :param kwargs: keyword argument
+        :param request: request argument dict type
+        :return:Sign up success page
+        """
+        username = None
+        password = None
+        if request.session.has_key('username') and request.session.has_key('password'):
+            username = request.session['username']
+            password = request.session['password']
+        jira_options = {'server': 'https://jira.kiwitech.com/', 'verify': True}
+        try:
+            jira = JIRA(options=jira_options, basic_auth=(username, password), max_retries=0)
+            projects = jira.projects()
+            return render(request, self.template_name, {
+                'user': request.user,
+                'projects': projects
+            })
+        except JIRAError as e:
+            logging.error("Failed to connect to JIRA: %s" % e)
+            return redirect('login')
 
 
+class LogoutView(View):
+    """
+    View is used for logout
+    """
+    @staticmethod
+    def get(request):
+        """
+        Method used to logout user
+        :param request: GET request
+        :return:redirect to login page
+        """
+        try:
+            del request.session['username']
+            del request.session['password']
+        except:
+            pass
+        return redirect('jira_login')
